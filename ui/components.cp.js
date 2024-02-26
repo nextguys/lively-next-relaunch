7136c8df4929711508d89141a544ea0c93910661
Robin Schreiber <robin.schreiber@me.com>
2024-02-26 18:18:15 +0100

    Created a first preliminary component for the website! Commited from within lively.next.

--- ui/components.cp.js
+++ ui/components.cp.js
@@ -1,1 +1,34 @@
-'format esm';
+import { component, TilingLayout } from 'lively.morphic';
+import { pt, rect } from 'lively.graphics/geometry-2d.js';
+import { Color } from 'lively.graphics/color.js';
+import { Image } from 'lively.morphic/morph.js';
+import { Text } from 'lively.morphic/text/morph.js';
+
+const LivelyFooter = component({
+  extent: pt(707.2, 108.7),
+  layout: new TilingLayout({
+    axisAlign: 'center',
+    padding: rect(20, 0, -20, 0),
+    spacing: 20
+  }),
+  submorphs: [{
+    type: Image,
+    name: 'lively logo',
+    extent: pt(77.6, 80.6),
+    fill: Color.rgb(255, 255, 255),
+    position: pt(36.9, 34)
+  }, {
+    type: Text,
+    name: 'impressum',
+    fontColor: Color.rgb(81, 90, 90),
+    fontFamily: 'Roboto',
+    fontSize: 19,
+    dynamicCursorColoring: true,
+    fill: Color.rgb(255, 255, 255),
+    position: pt(143.2, 62.8),
+    textAndAttributes: ['This is the ', null, 'lively.next', {
+      fontWeight: '500'
+    }, ' impressum. Or shall we drop it alltogether?', null]
+
+  }]
+});
