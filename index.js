0bc2b7c6a0204fd4bd5b16c42c5748f6ce932515
Robin Schreiber <robin.schreiber@me.com>
2024-07-12 17:59:49 +0200

    Fix bug where clicking on the page lead to its contents jumping

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { LivelyWebPage } from './ui/components.cp.js';
 import { pt } from 'lively.graphics';
 
 export async function main () {
+  $world.env.eventDispatcher.keyInputHelper.domState.textareaNode.setAttribute('disabled', true); // prevent jumping
   const website = part(LivelyWebPage, { extent: pt(window.innerWidth, window.innerHeight) });
   document.body.style.overflowX = 'hidden';
   document.body.style.overflowY = 'auto';
