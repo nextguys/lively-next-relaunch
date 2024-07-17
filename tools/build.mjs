aed252b9b986c4c1c80bf8cc773cb06edca34227
Robin Schreiber <robin.schreiber@me.com>
2024-07-17 18:12:45 +0200

    Update build script to accommodate the interactive documentation

--- tools/build.mjs
+++ tools/build.mjs
@@ -18,27 +18,17 @@ const build = await rollup({
         title: 'nextguys--lively-next-relaunch',
       },
       minify,
-      verbose,
+      verbose: true,
+      isResurrectionBuild: true,
       asBrowserModule: true,
       excludedModules: [
-	      'lively.collab',
+	'lively.collab',
         'mocha-es6','mocha', // references old lgtg that breaks the build
         'rollup', // has a dist file that cant be parsed by rollup
-        // other stuff that is only needed by rollup
-        '@babel/preset-env',
-        '@babel/plugin-syntax-import-meta',
         '@rollup/plugin-json', 
         '@rollup/plugin-commonjs',
         'rollup-plugin-polyfill-node',
-        'babel-plugin-transform-es2015-modules-systemjs',
-        'lively.ast',                                                  
-        'lively.vm',                                                    
-        'lively.ide',                                                   
-        'lively.modules',                                               
         'babel-plugin-transform-jsx',                                   
-        'lively-system-interface',                                      
-        'lively.storage',                                               
-        'lively.source-transform'
       ],
       resolver
     }),
