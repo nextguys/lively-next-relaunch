31575708db3ca3fd460beb2a89d936ca4c2b0884
Linus Hagemann <mail@linushagemann.de>
2024-07-22 16:02:27 +0200

    Fix youtube embedding being executed more than once

--- ui/pages/compiled_history.cp.js
+++ ui/pages/compiled_history.cp.js
@@ -2,11 +2,18 @@ import { component, TilingLayout, part, HTMLMorph } from 'lively.morphic';
 import { pt } from 'lively.graphics';
 import { CompiledHistoryPageDesktopHTML } from './compiled_desktop.js';
 import { CompiledHistoryPageMobileHTML } from './compiled_mobile.js';
+// We need to import here in order to ensure that the lib is present in the bundle
 import liteYouTubeEmbed from 'esm://cache/lite-youtube-embed'; // eslint-disable-line no-unused-vars
 
 // Browsers do not like us importing this in more than once place...
-// We need to import here in order to ensure that the lib is present in the bundle
-window.liteYouTubeEmbed = liteYouTubeEmbed;
+
+if (!window.liteYouTubeEmbed && !window.liteYoutubeLoading) {
+  window.liteYoutubeLoading = true;
+  System.import('lite-youtube-embed').then((embed) => {
+    window.liteYouTubeEmbed = embed;
+    delete window.liteYoutubeLoading;
+  });
+}
 
 export const CompiledHistoryPageDesktop = component({
   name: 'page',
@@ -44,11 +51,7 @@ export const CompiledHistoryPage = component({
         ]
       })]]
   },
-  // FIXME:
-  // This is a bit weird. Somehow, the HTML of the rendered HistoryPageHTML contains a hardcoded height of 2000.
-  // It is unclear where this comes from, as opening the HistoryPage as a plain Morph yields the correct height.
-  // However, putting a really high height here does resolve the issue. Otherwise, content of the history is cut-off.
-  extent: pt(900, 20000),
+  extent: pt(900, 1400),
   layout: new TilingLayout({
     hugContentsVertically: true,
     resizePolicies: [['wrapper', {
