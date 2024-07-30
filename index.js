14c72eedb76aca3540d630b7e8d192133cc941f4
Robin Schreiber <robin.schreiber@me.com>
2024-07-30 17:35:08 +0200

    Fix `show`, status message error and list issue

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-import { part, TilingLayout } from 'lively.morphic';
+import { part, World, TilingLayout } from 'lively.morphic';
 import { LivelyWebPage } from './ui/components.cp.js';
-import { pt } from 'lively.graphics';
+import { pt, Color } from 'lively.graphics';
+import { show } from 'lively.halos';
 
 export async function main () {
   const website = part(LivelyWebPage, { name: 'lively webpage', extent: pt(window.innerWidth, window.innerHeight) });
@@ -10,4 +11,24 @@ export async function main () {
   $world.layout = new TilingLayout({
     resizePolicies: [['lively webpage', { width: 'fill', height: 'fill' }]]
   });
+}
+
+export class WORLD_CLASS extends World {
+  setStatusMessageFor (aMorph, msg) {
+    console.log(msg);
+  }
+
+  moveIntoVisibleBounds () {
+    // just dont do anything
+  }
+
+  get commands () {
+    return [
+      ...super.commands, {
+        name: 'show morph',
+        exec: (world, { morph, loop = false, color = Color.red }) => {
+          return show(morph, loop, color);
+        }
+      }];
+  }
 }
