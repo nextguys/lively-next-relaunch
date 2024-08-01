83a19960fe5fc38db40adfa96cc674695fd6f71c
Linus Hagemann <mail@linushagemann.de>
2024-08-01 14:25:21 +0200

    Remove accidental test article from `Blog`

--- assets/articles/entries.js
+++ assets/articles/entries.js
@@ -1,13 +1,5 @@
 export const entries = [
     {
-        "title": "A test entry!",
-        "date": "2024-03-14T00:00:00.000Z",
-        "abstract": "This is a \nthird attempt\nat multiuline studd\n",
-        "slug": "asdfasdf",
-        "author": "@linusha",
-        "content": "<h1 id=\"h1-heading-8-\">h1 Heading 8-)</h1>\n<p>This shit even comes with support for emojis! 😄</p>\n<h2 id=\"h2-heading\">h2 Heading</h2>\n<h3 id=\"h3-heading\">h3 Heading</h3>\n<h4 id=\"h4-heading\">h4 Heading</h4>\n<h5 id=\"h5-heading\">h5 Heading</h5>\n<h6 id=\"h6-heading\">h6 Heading</h6>\n<h2 id=\"horizontal-rules\">Horizontal Rules</h2>\n<hr />\n<h2 id=\"emphasis\">Emphasis</h2>\n<p><strong>This is bold text</strong></p>\n<p><strong>This is bold text</strong></p>\n<p><em>This is italic text</em></p>\n<p><em>This is italic text</em></p>\n<p><del>Strikethrough</del></p>\n<h2 id=\"blockquotes\">Blockquotes</h2>\n<blockquote>\n  <p>Blockquotes can also be nested…</p>\n  <blockquote>\n    <p>…by using additional greater-than signs right next to each other…</p>\n    <blockquote>\n      <p>…or with spaces between arrows.</p>\n    </blockquote>\n  </blockquote>\n</blockquote>"
-    },
-    {
         "title": "A new home for the lively.next project!",
         "date": "2024-07-31T00:00:00.000Z",
         "abstract": "After some years of neglect, this entry marks the launch of a fresh website for the lively.next project.\nHowever, the project itself has commenced at a far greater pace over the last few years.\nIn this post, we will talk a bit about the current developments in the lively.next project, this website, and our vision for the future.\n",
